149627a8f3c5990da5ef58bf7b64edd90889f2d0
Adarsh Kumar Pal <paladarsh978@gmail.com>
2025-07-30 07:07:50 +0530

    adding MONGO_ATLAS dataBase

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/studentDB", {
+MONGO_URL="mongodb://127.0.0.1:27017/studentDB"
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
